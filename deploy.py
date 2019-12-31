152251548eba2dc83d2d7a3864e04e68ee53f6e5
Ram Raja <ram.rajagopalan@canopy.com>
2019-12-31 04:09:46 -0500

    Get version number of package

--- deploy.py
+++ deploy.py
@@ -33,12 +33,20 @@ def get_package_name():
         print("WRONG INPUT")
 
 
-def update_version(username, password, host, content_package, headers):
+def get_version(username, password, host, content_package, headers):
+    url = "http://{0}/etc/packages/com.canopygrowth.{1}/{1}-content.zip/jcr%3acontent/vlt%3adefinition.json".format(host, content_package)
+    r = requests.get(url, auth=HTTPBasicAuth(username, password), verify=False)
+    resp_json = json.loads(r.text)
+    version = int(resp_json['version'])
+    return version
+
+
+def update_version(username, password, host, content_package, version, headers):
     url = "http://{}/crx/packmgr/update.jsp".format(host)
     payload = {
         'packageName': '{}-content'.format(content_package),
         'groupName': 'com.canopygrowth.{}'.format(content_package),
-        'version': '2',
+        'version': version+1,
         'path': '/etc/packages/com.canopygrowth.{0}/{0}-content.zip'.format(content_package)
     }
     r = requests.post(url=url, auth=HTTPBasicAuth(username, password), params=payload, verify=False)
@@ -54,8 +62,10 @@ def main():
     prod = data['prod']
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0'}
     content_package = get_package_name()
-    print(content_package)
-    update_version(username, password, staging, content_package, headers)
+    content_package = 'brands'
+    version = get_version(username, password, staging, content_package, headers)
+    update_version(username, password, staging, content_package, version, headers)
+    print(version)
 
 
 if __name__ == '__main__':
